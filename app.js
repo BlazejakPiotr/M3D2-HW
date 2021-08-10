e6166cf11b24565e85729620c8f2598b96e634b3
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-10 16:02:56 +0200

    dynamically creating album nodes

--- app.js
+++ app.js
@@ -55,5 +55,22 @@ function getBehemoth() {
 }
 
 function displaySomething(body) {
-  console.log(body);
+  const getRecentlyPlayedContainer = document.getElementById("recently-played");
+  for (let i = 0; i < body["data"].length; i++) {
+    let createAlbumNode = document.createElement("div");
+    createAlbumNode.classList.add("col", "mb-4");
+    createAlbumNode.innerHTML = `  <div id="card1" class="card h-100">
+              <a href="#">
+                <img
+                  class="card-img-top"
+                  src="${body.data[i].album.cover_xl}"
+                  alt="${body.data[i].album.title}"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">${body.data[i].album.title}</h5>
+                </div></a
+              >
+            </div>`;
+    getRecentlyPlayedContainer.appendChild(createAlbumNode);
+  }
 }
